1541dd3ccf3d771623622895d5a4f91c4ee815e1
Joel Perez <joelness03@gmail.com>
2019-11-11 19:20:44 -0700

    step 7 complete

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -5,7 +5,26 @@
 import sys
 from cpu import *
 
-cpu = CPU()
+print(sys.argv)
 
-cpu.load()
-cpu.run()
+if not sys.argv[1]:
+    print("Error: no argument supplied")
+
+else:
+    instructions = [line.rstrip('\n') for line in open(sys.argv[1])]
+    # with open(sys.argv[1]) as instructions:
+    program = []
+    for line in instructions:
+        comment = line.find('#')
+        if comment >= 0:
+            line = line[:comment]
+        # print(line)
+        line = line.rstrip()
+        if len(line) == 8:
+            program.append(line)
+
+    # print(program)
+    cpu = CPU()
+
+    cpu.load(program)
+    cpu.run()
